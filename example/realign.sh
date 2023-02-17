dc83f33a51ef0717d1a0c264c2f9cd21150f4f59
David J. H. Shih <djh.shih@gmail.com>
2023-02-17 14:30:27 +0800

    Simplify annotation

--- example/realign.sh
+++ example/realign.sh
@@ -1,12 +1,13 @@
-#' Align paired-end sequencing reads
-#' @in  fastq1   file    read 1
-#' @in  fastq2   file    read 2
-#' @in  ref      file    reference sequence
-#' @in  bam      str     output file name
-#' @out bam      file
-#' @out bai      file
-#' @run cpu      int     [default: 2]
-#' @run memory   size    [default: 8G]
+#' doc Align paired-end sequencing reads
+#' in  fastq1   file                    | read 1
+#' in  fastq2   file                    | read 2
+#' in  ref      file                    | reference sequence
+#' in  outbase  str                     | output base name
+#' out bam      file  =  {outbase}.bam  | output alignment
+#' out bai      file  =  {outbase}.bai  | output alignment index
+#' run cpu    = 2
+#' run memory = 8G
+#' run image  = djhshih/seqkit:0.1
 
 bwa mem -t ${cpu} ${ref} ${fastq1} ${fastq2} | samtools view -b - > aligned.bam
 
