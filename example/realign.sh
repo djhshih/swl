74a86e33cd337d9b6fc2bd0d0f4add4491538aec
David J. H. Shih <djh.shih@gmail.com>
2023-02-17 14:37:59 +0800

    Fix string interpolation

--- example/realign.sh
+++ example/realign.sh
@@ -3,13 +3,13 @@
 #' in  fastq2   file                    | read 2
 #' in  ref      file                    | reference sequence
 #' in  outbase  str                     | output base name
-#' out bam      file  =  {outbase}.bam  | output alignment
-#' out bai      file  =  {outbase}.bai  | output alignment index
+#' out bam      file  =  ${outbase}.bam | output alignment
+#' out bai      file  =  ${outbase}.bai | output alignment index
 #' run cpu    = 2
 #' run memory = 8G
 #' run image  = djhshih/seqkit:0.1
 
 bwa mem -t ${cpu} ${ref} ${fastq1} ${fastq2} | samtools view -b - > aligned.bam
 
-samtools sort -@ ${cpu} -m ${memory / cpu} aligned.bam ${bam}
-samtools index ${bam} ${bai}
+samtools sort -@ ${cpu} -m ${memory / cpu} aligned.bam ${outbase}.bam
+samtools index ${outbase}.bam ${outbase}.bai
