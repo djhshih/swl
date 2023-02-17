8726dfe2e3120e76ce6210ab6dbe34a26afe0a19
David J. H. Shih <djh.shih@gmail.com>
2023-02-17 14:11:55 +0800

    Add annotation.

--- example/realign.sh
+++ example/realign.sh
@@ -1,4 +1,14 @@
+#' Align paired-end sequencing reads
+#' @in  fastq1   file    read 1
+#' @in  fastq2   file    read 2
+#' @in  ref      file    reference sequence
+#' @in  bam      str     output file name
+#' @out bam      file
+#' @out bai      file
+#' @run cpu      int     [default: 2]
+#' @run memory   int     [default: 8G]
+
 bwa mem -t ${cpu} ${ref} ${fastq1} ${fastq2} | samtools view -b - > aligned.bam
 
-samtools sort -@ ${cpu} -m ${memory/cpu} aligned.bam ${bam}
+samtools sort -@ ${cpu} -m ${memory / cpu} aligned.bam ${bam}
 samtools index ${bam} ${bai}
