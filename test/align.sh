3a4eeb7cfcfc0bb3df281a9fa5f3b4746a2172b7
David J. H. Shih <djh.shih@gmail.com>
2023-05-12 02:55:56 +0800

    Design a new annotation format.

--- test/align.sh
+++ test/align.sh
@@ -1,16 +1,21 @@
-#? Align paired-end sequencing reads
+## Align paired-end sequencing reads
 #
-# in  fastq1   file                    | read 1
-# in  fastq2   file                    | read 2
-# in  ref      file                    | reference sequence
-# in  ref_amb  file                    | reference bwa index file
-# in  ref_ann  file                    | reference bwa index file
-# in  ref_bwt  file                    | reference bwa index file
-# in  ref_pac  file                    | reference bwa index file
-# in  ref_sa   file                    | reference bwa index file
-# in  outbase  str                     | output base name
-# out bam      file  =  ${outbase}.bam | output alignment
-# run cpu      int   = 2
-# run image    size  = djhshih/seqkit:0.1
+# fun:
+#   fastq1, fastq2, ref, ref_amb, ref_ann, ref_bwt, ref_pac, ref_sa -> bam
+#
+# in:
+#   fastq1, fastq2 file | paired-end reads
+#   ref file | reference sequence
+#   ref_amb, ref_ann, ref_bwt, ref_pac, ref_sa file
+#     | reference bwa index files
+#   outbase str | output base name
+#
+# out:
+#   bam file = ${outbase}.bam
+#     | output alignment
+#
+# run:
+#   cpu int = 2
+#   image size = djhshih/seqkit:0.1
 
 bwa mem -t ${cpu} ${ref} ${fastq1} ${fastq2} | samtools view -b - > ${outbase}.bam
