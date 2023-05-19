42c43d1a73526aedb762b431fe65eaca5a0ebe9c
David J. H. Shih <djh.shih@gmail.com>
2023-05-20 00:19:40 +0800

    Continue implementing task parser.

--- python/swl/syntax/task.py
+++ python/swl/syntax/task.py
@@ -4,75 +4,106 @@ keywords = ['fun', 'in', 'out', 'run']
 keyword_phase_start = 10
 
 def match_keyword(s):
+    '''Return the matched keyword or None if no match.'''
     for i in range(len(keywords)):
         keyword = keywords[i]
-        if s.startswith(keyword + ':'):
-            return i
-    return -1
+        if s.strip() == keyword + ':'
+            return keyword
+    return None
+
+class EndOfAnnotation(Exception):
+    '''Raised when end of annotation is encountered'''
+    pass
+
+class Parameter:
+    '''Parameter for a task.'''
+    def __init__(self, name, _type = 'file', doc = ''):
+        self.name = name
+        self.type = _type
+        self.doc = doc
 
 class ShellTask:
 
     def __init__(self, f: io.TextIOBase):
         '''Create a callable function from an annotated shell script.'''
-        self._parse(f)
+        self.iter = iter(f)
+        self.i = 0
+        self._parse()
 
-    def _parse(self, f: ioTextIOBase):
-        '''Parse an annotated shell script.''' 
-        it = iter(f):
+    def _size(self) -> int:
+        '''Remaining size of input string'''
+        return len(self.lines) - self.i
 
-        line_num = 0
-
-        # parse title
-        phase = 0
-        for line in it:
-            line_num += 1
-            if line.startswith('##'):
-                # start parsing description
-                self.title = line[2:].strip()
-                phase = 1
-                continue
-            if phase > 0:
-                if line.startswith('#'):
+    def _next_line(self, prefix=None) -> str:
+        while True:
+            self.i += 1
+            line = next(self.iter)
+            if prefix:
+                if line.startswith(prefix):
                     s = line[1:].strip()
                     if len(s) > 0:
-                        self.title += s
-                    else:
-                        break
-            
-        # parse description
-        self.description = ''
-        for line in it: 
-            line_num += 1
-            if line.startswith('#'):
-                s = line[1:].strip()
-                if len(s) > 0:
-                    # description is optional
-                    # quit parsing description if keyword is encountered
-                    i = match_keyword(s)
-                    if i >= 0:
-                        phase = keyword_phase_start + i
-                        break
-                    self.description += s
+                        return s
+                else:
+                    raise EndAnnotation
+            else:
+                if len(line) > 0:
+                    return line
 
-        # parse keyword-marked sections
-        keyword = None
-        for line in it:
-            line_num += 1
-            if line.startswith('#'):
-                s = line[1:].strip()
-                if len(s) > 0:
-                    i = match_keyword(s):
-                    if i < 0:
-                        raise ValueError(
-                            f"Expecting keyword but none found on line {line_num}."
-                        )
-                    else:
-                        self._parse_section(it, keyword)
-    
-    def _parse_section(self, it, section):
-        if section == 'fun':
-            # TODO
+    def _parse(self):
+        '''Parse an annotated shell script.''' 
+
+        try:
+            # parse title
+            line = _next_line('##')
+            self.title = line[2:].strip()
+            
+            # parse description,
+            # which includes all lines until the next section
+            self.description = ''
+            k = 0
+            while True:
+                line = _next_line('#')
+                section = match_keyword(line)
+                if section:
+                    break
+                else:
+                    self.description += line
+
+            # parse each section
+            while True:
+                self._parse_section(keywords[k])
+                line = _next_line('#')
+                section = match_keyword(line)
+                if not section:
+                    raise ValueError(
+                        f"Expecting keyword but none found on line {self.i}."
+                    )
+
+        except EndAnnotation:
             pass
+
+        except StopIteration:
+            pass
+
+        # unpack the remaining lines
+        self.body = [*self.iter]
+    
+    def _parse_section(self, section):
+        line = _next_line('#')
+
+        next_section = match_keyword(line)
+        if next_section:
+            self._parse_section(next_section)
+            return
+
+        if section == 'fun':
+            try:
+                line = line.strip()
+                parts = line.split('->')
+                self.inputs  = [Parameter(x.strip()) for x in parts[0].split(',')]
+                self.outputs = [Parameter(x.strip()) for x in parts[1].split(',')]
+            except:
+                raise ValueError(f"fun section is malformed:\n{line}")
         elif section == 'in':
             # TODO
             pass
