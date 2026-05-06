17fd3508fb228e9bee04d0ad868eb2bd0874bf90
David J. H. Shih <djh.shih@gmail.com>
2026-05-06 23:56:31 +0800

    Add tests.

--- test.sh
+++ test.sh
@@ -1,16 +1,43 @@
 #!/bin/bash
 
-evaluate() {
+set -e
+
+run_unit_tests() {
+	echo "running unit tests"
+	PYTHONPATH=python python -m unittest \
+		swl.syntax.lexer \
+		swl.syntax.parser \
+		swl.syntax.task.test_parser \
+		swl.syntax.task.test_interpolation \
+		swl.syntax.task.test_bash
+	printf "unit tests passed\n\n"
+}
+
+evaluate_task() {
+	echo "file: $1"
+	PYTHONPATH=python python -m swl.eval_task $1
+	printf "return: $?\n\n"
+}
+
+evaluate_wf() {
 	echo "file: $1"
 	PYTHONPATH=python python -m swl.eval $1
 	printf "return: $?\n\n"
 }
 
 if (( $# > 0 )); then
-	evaluate $1
+	if [[ $1 =~ \.swl ]]; then
+		evaluate_wf $1
+	else
+		evaluate_task $1
+	fi
 else
+	run_unit_tests
+	for task in tests/*.sh; do
+		evaluate_task $task
+	done
 	for swl in tests/*.swl; do
-		evaluate $swl
+		evaluate_wf $swl
 	done
 fi
 
