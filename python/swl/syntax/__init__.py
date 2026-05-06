f5525ff1a1711ed5080e917afc6bea382e4c8062
David J. H. Shih <djh.shih@gmail.com>
2026-05-06 22:38:30 +0800

    Rename script.

--- python/swl/syntax/__init__.py
+++ python/swl/syntax/__init__.py
@@ -3,4 +3,4 @@ from swl.syntax.wf import lexer
 from swl.syntax.wf import parser
 from swl.syntax.task import annotation
 from swl.syntax.task import parser
-from swl.syntax.task import node
+from swl.syntax.task import type
