6c36b3b6ed62cc442a05fdf3b43a6e027f675606
David J. H. Shih <djh.shih@gmail.com>
2026-06-11 12:44:11 +0800

    Fix stub test.

--- tests/integration/swl/sort.sh
+++ tests/integration/swl/sort.sh
@@ -5,8 +5,8 @@
 #   outbase str | output base name
 #
 # out
-#   bam file = ${outbase}.bam | output
+#   bam file = ${outbase}.sorted.bam | output
 #   bai file = ${outbase}.bai | index
 
-cp "${bam}" "${outbase}.bam"
+cp "${bam}" "${outbase}.sorted.bam"
 cp "${bam}" "${outbase}.bai"
